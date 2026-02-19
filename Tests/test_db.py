f6e4afda2aed9e44c619f75e40b166f1de765587
AndreLopes30 <ferreiraandre833@gmail.com>
2026-02-19 16:31:20 -0300

    Correção de identação

--- Tests/test_db.py
+++ Tests/test_db.py
@@ -3,7 +3,7 @@ from app.db.database import engine
 if __name__ == "__main__":
     try:
         connection = engine.connect()
-       print("✅ Conectou no PostgreSQL com sucesso!")
-       connection.close()
+        print("✅ Conectou no PostgreSQL com sucesso!")
+        connection.close()
     except Exception as e:
         print("❌ Erro ao conectar:", e)
